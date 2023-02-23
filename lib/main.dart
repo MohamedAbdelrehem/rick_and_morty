375fbc8e3c41309ce6d95c31bc12f6b0d955700b
MohamedAbdelrehem <mohamedabdelrehem32@gmail.com>
2023-02-23 12:59:33 +0200

    Start

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RickAndMortyApp());
+}
+
+class RickAndMortyApp extends StatelessWidget {
+  const RickAndMortyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
