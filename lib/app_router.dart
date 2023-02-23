c9d338a01ae222868ab7a53ffadcbb376b504ae1
MohamedAbdelrehem <mohamedabdelrehem32@gmail.com>
2023-02-23 20:58:03 +0200

    add characters backup data and setting it up

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/presentation/screens/character_screen.dart';
+import 'package:rick_and_morty/presentation/screens/characters_details_screen.dart';
+
+import 'Constants/strings.dart';
 
 class AppRouter {
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case '/':
+      case charactersScreen:
         return MaterialPageRoute(builder: (_) => CharactersScreen());
+      case characterDetailsScreen:
+        return MaterialPageRoute(builder: (_) => CharactersDetailsScreen());
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rick_and_morty/app_router.dart';
 
 void main() {
-  runApp(RickAndMortyApp(
-    appRouter: AppRouter(),
-  ));
+  runApp(RickAndMortyApp(appRouter: AppRouter()));
 }
 
 class RickAndMortyApp extends StatelessWidget {
@@ -15,9 +13,9 @@ class RickAndMortyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      onGenerateRoute: appRouter
-          .generateRoute, // we used the buildin function in the variable that called generateRoute to
-    );
+        debugShowCheckedModeBanner: false,
+        onGenerateRoute: appRouter
+            .generateRoute // we used the buildin function in the variable that called generateRoute to
+        );
   }
 }
