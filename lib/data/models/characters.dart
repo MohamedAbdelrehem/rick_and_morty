c72f303d92a9cec6162151a0bc1ae3ab8a2da755
MohamedAbdelrehem <mohamedabdelrehem32@gmail.com>
2023-02-24 20:01:37 +0200

    add characters backup data and setting it up

--- lib/data/models/characters.dart
+++ lib/data/models/characters.dart
@@ -13,7 +13,8 @@ class Character {
     name = json["name"];
     deadOrAlive = json["status"];
     humanOrAlian = json["species"];
-    features= json["type"];
-    gender= json["gender"];
-    image= json["image"];
+    features = json["type"];
+    gender = json["gender"];
+    image = json["image"];
   }
+}
